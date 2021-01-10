119670152b2eeb9b93315c9219cb6bf5014ec09d
Dylan Authentic <dylan@authentic.tech>
2021-01-09 22:13:34 -0500

    Initial Commit

---
+++ app.py
@@ -0,0 +1,52 @@
+import dash
+import dash_core_components as dcc # python abstraction for react.js and other interactive user interfaces
+import dash_html_components as html # python wrapper for html elements
+import pandas as pd
+
+# read and preprocess data for use in dashboard
+data = pd.read_csv("data.csv")
+data = data.query("type == 'conventional' and region == 'Albany'")
+data["Date"] = pd.to_datetime(data["Date"], format="%Y-%m-%d")
+data.sort_values("Date", inplace=True)
+
+# initialize app
+app = dash.Dash(__name__)
+
+# define the layout property of the app object
+app.layout = html.Div(
+    children=[
+        html.H1(children="Avocado Analytics",),
+        html.P(
+            children="Analyze the behavior of avocado prices"
+            " and the number of avocados sold in the US"
+            " between 2015 and 2018",
+        ),
+        dcc.Graph(
+            figure={
+                "data": [
+                    {
+                        "x": data["Date"],
+                        "y": data["AveragePrice"],
+                        "type": "lines",
+                    },
+                ],
+                "layout": {"title": "Average Price of Avocados"},
+            },
+        ),
+        dcc.Graph(
+            figure={
+                "data": [
+                    {
+                        "x": data["Date"],
+                        "y": data["Total Volume"],
+                        "type": "lines",
+                    },
+                ],
+                "layout": {"title": "Avocados Sold"},
+            },
+        ),
+    ]
+)
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
